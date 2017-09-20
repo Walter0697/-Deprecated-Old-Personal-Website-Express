aaeace2b9c559d80c645ca73d0f8089bbd040599
Walter Cheng <walterpakwai@live.com>
2017-09-20 15:27:04 -0400

    small changes

--- server.js
+++ server.js
@@ -10,14 +10,26 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 
  //catch all requests and log them
-app.use('*', function(req, res, next){
+/*app.use('*', function(req, res, next){
    console.log(req.url+' request for '+req.url);
    next(); //allow next route or middleware to run
-});
+});*/
 
 //setup the index page
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/portfolio.html');
+});
+
+app.get('/about', function(req, res){
+	res.sendFile(__dirname + '/public/about.html');
+});
+
+app.get('/project', function(req, res){
+	res.sendFile(__dirname + '/public/project.html');
+});
+
+app.get('/resume', function(req, res){
+	res.sendFile(__dirname + '/public/resume.html');
 });
 
 app.get("*", function(req, res){
