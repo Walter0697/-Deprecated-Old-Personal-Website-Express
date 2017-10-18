7e600c4fc8637efdbf5c1350f3d31142b4215fcb
Walter Cheng <walterpakwai@live.com>
2017-10-18 13:59:20 -0400

    typo

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 //loading nescessary modules and setting up constant value
 const PORT = process.env.PORT || 80;
-//const ipAddress = "localhost";
-const ipAddress = "45.77.110.134";
+const ipAddress = "localhost";
+//const ipAddress = "45.77.110.134";
 const express = require('express');
 const app = express();
 
