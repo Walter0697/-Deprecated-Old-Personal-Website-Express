d0088d67425c057225fb80233a71d9991b396f8b
Walter0697 <walterpakwai@live.com>
2018-10-08 01:34:26 -0400

    do it

--- server.js
+++ server.js
@@ -147,10 +147,12 @@ app.post('/tech', function(req, res){
 
 //puzzle game
 app.get('/puzzle/:keycode', function(req, res){
+	console.log("someone played puzzle with" + req.params.keycode);
 	res.sendFile(__dirname + '/public/puzzle/' + req.params.keycode + ".html");
 });
 
 app.get('/jay/:keycode', function(req, res){
+	console.log("someone played jay with" + req.params.keycode);
 	res.sendFile(__dirname + '/public/jay/' + req.params.keycode + ".html");
 });
 
